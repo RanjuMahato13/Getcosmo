004c6c470433b14c35c1a722416bca022bd929c9
Ranjana Mahato <np03cs4s220009@heraldcollege.edu.np>
2024-03-21 11:05:30 +0545

    initial commit

---
+++ routes/blogRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog, likeBlog, dislikeBlog } = require("../controller/blogCtrl");
+const { uploadPhoto, blogImgResize } = require("../middlewares/uploadImages");
+const { uploadImages } = require("../controller/blogCtrl");
+const router = express.Router();
+
+
+router.post("/", authMiddleware, isAdmin, createBlog);
+router.put(
+    "/upload/:id",
+    authMiddleware,
+    isAdmin,
+    uploadPhoto.array("images", 10),
+    blogImgResize,
+    uploadImages
+)
+router.put("/likes", authMiddleware, likeBlog);
+router.put("/dislikes", authMiddleware, dislikeBlog);
+
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.get("/:id", getBlog);
+router.get("/", getAllBlogs);
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog);
+
+
+module.exports = router; 
