ea05eff57ed111f0fe272ac141ae164ccee383bc
Ranjana Mahato <np03cs4s220009@heraldcollege.edu.np>
2024-05-13 15:07:09 +0545

    Add checkout using khalti payment

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,35 +1,36 @@
 const express = require("express");
-const { 
-    createUser, 
-    loginUserCtrl, 
-    getallUser,
-    getaUser,
-    deleteaUser,
-    updatedUser,
-    blockUser,
-    unblockUser,
-    handleRefreshToken,
-    logout,
-    updatePassword,
-    forgotPasswordToken,
-    resetPassword,
-    loginAdmin,
-    getWishlist,
-    saveAddress,
-    userCart,
-    getUserCart,
-    //checkout,
-    emptyCart,
-    applyCoupon,
-    createOrder,
-    getOrders,
-    updateOrderStatus,
-    getAllOrders,
-    removeProductFromCart,
-    updateProductQuantityFromCart,
-   // getMyOrders
+const {
+  createUser,
+  loginUserCtrl,
+  getallUser,
+  getaUser,
+  deleteaUser,
+  updatedUser,
+  blockUser,
+  unblockUser,
+  handleRefreshToken,
+  logout,
+  updatePassword,
+  forgotPasswordToken,
+  resetPassword,
+  loginAdmin,
+  getWishlist,
+  saveAddress,
+  userCart,
+  getUserCart,
+  //checkout,
+  emptyCart,
+  applyCoupon,
+  createOrder,
+  getOrders,
+  updateOrderStatus,
+  getAllOrders,
+  removeProductFromCart,
+  updateProductQuantityFromCart,
+  getMyOrders,
+  // getMyOrders
 } = require("../controller/userCtrl");
-const { authMiddleware, isAdmin} = require("../middlewares/authMiddleware");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 //const { paymentVerification, checkout } = require("../controller/paymentCtrl");
 const router = express.Router();
 router.post("/register", createUser);
@@ -45,27 +46,32 @@ router.post("/cart", authMiddleware, userCart);
 //router.post("/cart/applycoupon", authMiddleware, applyCoupon);
 router.post("/cart/create-order", authMiddleware, createOrder);
 router.get("/all-users", getallUser);
-// router.get("/get-orders", authMiddleware, getOrders);  yo remove gardini paxi chaidiana
-//router.get("/getmyorders", authMiddleware, getMyOrders);
-// router.get("/getallorders", authMiddleware, isAdmin, getAllOrders);
-// router.post("/getorderbyuser/:id", authMiddleware, isAdmin, getAllOrders);
+// router.get("/get-orders", authMiddleware, getOrders);   umm chaidain
+router.get("/getmyorders", authMiddleware, getMyOrders);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
 router.get("/wishlist", authMiddleware, getWishlist);
 router.get("/cart", authMiddleware, getUserCart);
 
-router.get("/:id", authMiddleware,isAdmin, getaUser);
+router.get("/:id", authMiddleware, isAdmin, getaUser);
 //router.delete("/empty-cart", authMiddleware, emptyCart);
-router.delete("/delete-product-cart/:cartItemId", authMiddleware, removeProductFromCart);
-router.delete("/update-product-cart/:cartItemId/:newQuantity", authMiddleware, updateProductQuantityFromCart);
+router.delete(
+  "/delete-product-cart/:cartItemId",
+  authMiddleware,
+  removeProductFromCart
+);
+router.delete(
+  "/update-product-cart/:cartItemId/:newQuantity",
+  authMiddleware,
+  updateProductQuantityFromCart
+);
 router.delete("/empty-cart", authMiddleware, emptyCart);
 router.delete("/:id", deleteaUser);
 // router.put("/order/update-order/:id", authMiddleware, isAdmin, updateOrderStatus);
 
-router.put("/edit-user",authMiddleware, updatedUser);
-router.put("/save-address",authMiddleware, saveAddress);
-router.put("/block-user/:id",authMiddleware, isAdmin, blockUser);
-router.put("/unblock-user/:id",authMiddleware, isAdmin, unblockUser);
-
+router.put("/edit-user", authMiddleware, updatedUser);
+router.put("/save-address", authMiddleware, saveAddress);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
 
 module.exports = router;
